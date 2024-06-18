604d928e2f3f3fd35762ee9b13969f0b02c3607b
CleanerCape <138161325+CleanerCape@users.noreply.github.com>
2024-06-18 14:53:54 +0200

    Städat upp gamla kommentarer

--- gif_converter.py
+++ gif_converter.py
@@ -7,10 +7,8 @@ file_names = []
 
 for file in files:
     file_name = str(file)
-    #file_name_short = file_name[:-4]
     file_names.append(file_name)
 
 for file_name in file_names:
-    #convert_video(file_name)
     videoClip = VideoFileClip(file_name)
     videoClip.write_gif(file_name[:-4] + ".gif",fps=videoClip.fps,program="ffmpeg")
